b5f871c22f905c6ef14bd68a2f099ea177c7ea2f
Soban-2004 <165470467+Soban-2004@users.noreply.github.com>
2025-09-08 23:38:31 +0530

    Update stop_words.py

--- core/stop_words.py
+++ core/stop_words.py
@@ -1,7 +1,9 @@
 import nltk
 from nltk.corpus import stopwords
+import streamlit as st
 
 @st.cache_resource
 def load_stopwords():
     nltk.download("stopwords")
+
     return set(stopwords.words("english"))
