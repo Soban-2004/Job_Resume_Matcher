dd2eb847d87912a7b2c3a4450cf628b435cead79
Soban-2004 <165470467+Soban-2004@users.noreply.github.com>
2025-09-08 23:38:54 +0530

    Update embeddings.py

--- core/embeddings.py
+++ core/embeddings.py
@@ -1,5 +1,7 @@
 from langchain_huggingface import HuggingFaceEmbeddings
+import streamlit as st
 
 @st.cache_resource
 def get_hugging_face_embeddings():
     return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
